7e3b47a8fe9113f1ecbee0a544410a53745a690c
Lino Barreca <L.Barreca@GBnet.it>
2019-10-22 11:49:52 +0200

    Updated soft serial pins according to BTT schema

--- Marlin/src/pins/stm32/pins_BIGTREE_SKR_PRO_V1.1.h
+++ Marlin/src/pins/stm32/pins_BIGTREE_SKR_PRO_V1.1.h
@@ -137,22 +137,22 @@
   //
   // Software serial
   //
-  #define X_SERIAL_TX_PIN  PC13
+  #define X_SERIAL_TX_PIN  PE4
   #define X_SERIAL_RX_PIN  PC13
 
-  #define Y_SERIAL_TX_PIN  PE3
+  #define Y_SERIAL_TX_PIN  PE2
   #define Y_SERIAL_RX_PIN  PE3
 
-  #define Z_SERIAL_TX_PIN  PE1
+  #define Z_SERIAL_TX_PIN  PE0
   #define Z_SERIAL_RX_PIN  PE1
 
-  #define E0_SERIAL_TX_PIN PD4
+  #define E0_SERIAL_TX_PIN PD2
   #define E0_SERIAL_RX_PIN PD4
 
-  #define E1_SERIAL_TX_PIN PD1
+  #define E1_SERIAL_TX_PIN PD0
   #define E1_SERIAL_RX_PIN PD1
 
-  #define E2_SERIAL_TX_PIN PD6
+  #define E2_SERIAL_TX_PIN PD5
   #define E2_SERIAL_RX_PIN PD6
 #endif
 
@@ -207,13 +207,6 @@
     #define LCD_PINS_ENABLE PG7
     #define LCD_PINS_D4    PG3
 
-    // CR10_Stock Display needs a different delay setting on SKR PRO v1.1, so undef it here. 
-    // It will be defined again at the #HAS_GRAPHICAL_LCD section below.
-    #undef ST7920_DELAY_1
-    #undef ST7920_DELAY_2
-    #undef ST7920_DELAY_3
-
-
   #else
 
     #define LCD_PINS_RS    PD10
@@ -226,26 +219,6 @@
 
     #define LCD_PINS_ENABLE PD11
     #define LCD_PINS_D4    PG2
-
-    #if ENABLED(FYSETC_MINI_12864)
-      #define DOGLCD_CS    PD11
-      #define DOGLCD_A0    PD10
-      //#define LCD_BACKLIGHT_PIN -1
-      #define LCD_RESET_PIN PG2   // Must be high or open for LCD to operate normally.
-      #if EITHER(FYSETC_MINI_12864_1_2, FYSETC_MINI_12864_2_0)
-        #ifndef RGB_LED_R_PIN
-          #define RGB_LED_R_PIN PG3
-        #endif
-        #ifndef RGB_LED_G_PIN
-          #define RGB_LED_G_PIN PG6
-        #endif
-        #ifndef RGB_LED_B_PIN
-          #define RGB_LED_B_PIN PG7
-        #endif
-      #elif ENABLED(FYSETC_MINI_12864_2_1)
-        #define NEOPIXEL_PIN    PG3
-      #endif
-    #endif // !FYSETC_MINI_12864
 
     #if ENABLED(ULTIPANEL)
       #define LCD_PINS_D5  PG3
